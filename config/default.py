e0ef336e09af314145d3f21dabcf617e7497c88f
Zoa <zhousz@mama.cn>
2015-05-02 16:34:49 +0800

    [新增]邮件发送类

--- config/default.py
+++ config/default.py
@@ -4,4 +4,11 @@ __author__ = 'Zoa Chou'
 
 
 class Config(object):
-    pass
+    # 邮件通知设置
+    MAIL_SMTP_SERVER = 'smtp.qq.com'
+    MAIL_SMTP_PORT = None
+    MAIL_SMTP_USER = 'youremail@qq.com'
+    MAIL_SMTP_PASSWORD = 'your email password'
+    MAIL_SMTP_SSL = True
+    MAIL_FROM_ADDRESS = 'Mailer admin<youremail@qq.com>'
+    MAIL_SUBJECT = 'your email title'
